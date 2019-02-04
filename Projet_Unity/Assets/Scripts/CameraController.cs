286b19d302735a3415143a82cff6902806ea3fd7
Unknown <matthieu.manuellan@gmail.com>
2019-02-04 12:02:04 +0100

    Setup Player & Camera

---
+++ Projet_Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.MouseX;
+
+    public const float Y_MIN = -45.0f;
+    public const float Y_MAX = 45.0f;
+
+    public float Sensitivity_x = 10.0f;
+    public float Sensitivity_y = 10.0f;
+
+    public float _rotationX = 0;
+
+    
+    void Update()
+    {
+        if (axes == RotationAxis.MouseX)
+            transform.Rotate(0, Input.GetAxis("Mouse X") * Sensitivity_y, 0);
+        else if (axes == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * Sensitivity_y;
+            _rotationX = Mathf.Clamp(_rotationX, Y_MIN, Y_MAX);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        
+    }
+}
---
+++ Projet_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _speed = 10.0f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x * _speed, 0, y * _speed);
+        move *= Time.deltaTime;
+        transform.Translate(move);
+    }
+}
