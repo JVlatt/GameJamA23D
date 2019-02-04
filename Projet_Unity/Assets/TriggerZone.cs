443245854c42962565366dc43dc3a45968adcb77
Unknown <matthieu.manuellan@gmail.com>
2019-02-04 15:09:20 +0100

    Hint

---
+++ Projet_Unity/Assets/TriggerZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public LayerMask _layer;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, _layer))
+            {
+                if (hit.transform.parent == transform)
+                    Debug.Log("c'est le bon");
+                else
+                    Debug.Log("c'est pas le bon");
+            }
+        }
+    }
+}
