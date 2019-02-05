f4c927d9f219e7ba60c665c2c966a369d0217cef
Unknown <cedric.chauvin99@gmail.com>
2019-02-05 15:24:35 +0100

    Code Image

---
+++ Projet_Unity/Assets/Scripts/CodeImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeImage : MonoBehaviour
+{
+    public string code;
+    private int[] num = {1,1,1};
+
+    public void Add(int numero)
+    {
+        num[numero]++;
+        if (num[numero] > 4) num[numero] = 1;
+    }
+
+    public void Soustract(int numero)
+    {
+        num[numero]--;
+        if (num[numero] < 1) num[numero] = 4;
+    }
+
+    public void Unlock()
+    {
+        string test = num[0].ToString() + num[1].ToString() + num[2].ToString();
+        if (test == code) Debug.Log("good");
+    }
+}
