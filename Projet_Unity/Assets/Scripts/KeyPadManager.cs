7d3fd411c5eec0109328240d46f449612fe1b43f
Unknown <matthieu.manuellan@gmail.com>
2019-02-05 08:57:09 +0100

    DigiCode Done

---
+++ Projet_Unity/Assets/Scripts/KeyPadManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyPadManager : MonoBehaviour
+{
+    public InputField TextField;
+    public string Password = "0000";
+
+
+
+    public void Set(int digit)
+    {
+        TextField.text += digit;
+    }
+
+    public void Unlock()
+    {
+        if (TextField.text == Password)
+        {
+            Debug.Log("Porte déverouillée");
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            TextField.text = "";
+            Debug.Log("Mot de Passe incorrect");
+        }
+    }
+
+    public void Reset()
+    {
+        TextField.text = "";
+    }
+
+
+}
