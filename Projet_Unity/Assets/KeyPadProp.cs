44e3f1d9bbdfa6306ee3e8c28e581bf9cab2b8ce
Unknown <matthieu.manuellan@gmail.com>
2019-02-06 17:56:10 +0100

    GameController

---
+++ Projet_Unity/Assets/KeyPadProp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPadProp : MonoBehaviour
+{
+    public GameObject KeyPadUI;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            other.GetComponent<PlayerController>()._frozen = true;
+            other.GetComponent<CameraController>().Block(true);
+            KeyPadUI.SetActive(true);
+        }
+    }
+}
---
+++ Projet_Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController _gameController;
+    private Transform _player;
+    private Transform _camera;
+    private PlayerController _playerController;
+    private CameraController _cameraController;
+
+
+    private void Awake()
+    {
+        _gameController = this;
+        _player = Camera.main.transform.parent;
+        _camera = Camera.main.transform;
+        _playerController = _player.GetComponent<PlayerController>();
+        _cameraController = _camera.GetComponent<CameraController>();
+    }
+
+    public void Freeze(bool state)
+    { 
+        _playerController._frozen = state;
+        _cameraController.Block(state);    
+    }
+}
