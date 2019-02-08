15d897af9f4412dcd4dcd34a9f5ef49623c4b130
Unknown <matthieu.manuellan@gmail.com>
2019-02-08 11:59:27 +0100

    Screen Script

---
+++ Projet_Unity/Assets/ScreenTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTrigger : MonoBehaviour
+{
+    public GameObject Image;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !Image.activeInHierarchy)
+            { 
+                Image.SetActive(true);
+                SoundControler._soundControler.PlaySound(SoundControler._soundControler._pcOn);
+            }
+        }
+
+    }
+}
---
+++ Projet_Unity/Assets/ScreenTriggerZone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenTriggerZone : MonoBehaviour
+{
+    public LayerMask _layer;
+    public float cooldown;
+    public float maxOppacité;
+    public Image feedback;
+    private float timer;
+    private bool detected;
+    public GameObject Screen1;
+    public GameObject Screen2;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !detected)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, _layer))
+            {
+                if (hit.transform.parent == transform && Screen1.activeInHierarchy && Screen2.activeInHierarchy)
+                {
+                    if (timer > 0) timer -= Time.deltaTime;
+                    else
+                    {
+                        SoundControler._soundControler.PlaySound(SoundControler._soundControler._vision);
+                        detected = true;
+                        timer = 2;
+                    }
+                }
+            }
+            else if (timer < cooldown) timer = cooldown;
+            Color color = feedback.color;
+            color.a = Mathf.Lerp(maxOppacité, 0, timer);
+            feedback.color = color;
+        }
+    }
+}
