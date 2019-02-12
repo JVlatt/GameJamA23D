966dd2f884c5f710e1f3aaef7f34b0ff4de1b27f
Ulysse <ulysse.raillon@hotmail.fr>
2019-02-12 23:50:19 +0100

    Lights 100%, Lamp, ceiling. LEFT : Collider '0' / Ajust image blue logo, sensi door, sound maze enter n error SC2 + add fun audio.

---
+++ Projet_Unity/Assets/Scripts/BlinkingLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLights : MonoBehaviour
+{
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+
+        IEnumerator Flashing ()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1.0f);
+                light.enabled = !light.enabled;
+            }
+        }
+    }
+}
