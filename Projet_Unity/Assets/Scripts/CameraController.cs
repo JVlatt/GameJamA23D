6f232bfdf17695971ed7f671734f1d32250e06d9
RAILLON <ulysse.raillon@hotmail.fr>
2019-02-13 12:32:49 +0100

    Lights, collider

--- Projet_Unity/Assets/Scripts/CameraController.cs
+++ Projet_Unity/Assets/Scripts/CameraController.cs
@@ -14,7 +14,7 @@ public class CameraController : MonoBehaviour
     public RotationAxis axes = RotationAxis.MouseX;
 
     public const float Y_MIN = -80.0f;
-    public const float Y_MAX = 60.0f;
+    public const float Y_MAX = 75.0f;
 
     public float Sensitivity_x = 10.0f;
     public float Sensitivity_y = 10.0f;
